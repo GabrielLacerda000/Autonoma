7313e7cd5b529dac15729094f210ae733294159d
GabrielLacerda000 <gabrielglacerda000@gmail.com>
2026-05-11 15:39:04 -0300

    feat: added class valdiator and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { startContentWorker } from './content-pipeline/processors/content.processor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Autônoma API')
+    .setDescription('The Autônoma API description')
+    .setVersion('1.0')
+    .addTag('content')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
   startContentWorker();
   console.log('👷 Worker running...');
